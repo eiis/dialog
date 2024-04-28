f450e6138fb423c605d5578164f732c0de949ab2
zhangdi <1258956799@qq.com>
2024-04-28 14:34:38 +0800

    update dev10

--- src/utils/dialog.ts
+++ src/utils/dialog.ts
@@ -13,6 +13,7 @@ export function dialog(component:any, props={},modalProps={}) {
     //dev 第七次提交的代码
     //dev 第八次提交的代码
     //dev 第九次提交的代码
+    //dev 第十次提交的代码
     render() {
       return h(Modal, {
         open: true,
